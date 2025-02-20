07af5740ca1aaba62d564116409e016bf48ae4b4
Mikhail <misha170801planshet@gmail.com>
2025-02-20 02:28:09 +0100

    Working test with simple post form

--- aboutMe/templatetags/test_tags.py
+++ aboutMe/templatetags/test_tags.py
@@ -1,9 +1,13 @@
 from django import template
 from aboutMe.models import Question, Choice
+import random
+
 
 register = template.Library()
 
 
 @register.simple_tag(name='latest_question_list1')
 def get_questions():
-    return Question.objects.all()
+    questions = list(Question.objects.all())
+    random.shuffle(questions)
+    return questions
--- aboutMe/views.py
+++ aboutMe/views.py
@@ -2,7 +2,7 @@ from datetime import timezone
 
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request):
@@ -14,6 +14,21 @@ def index(request):
 def test_yourself(request):
     #latest_question_list = Question.objects.all()
     #context = {"latest_question_list": latest_question_list}
+    questions = list(Question.objects.all())
+    if request.method == "POST":
+        correct_count = 0
+        total_questions = len(questions)
+
+        for question in questions:
+            selected_choice_id = request.POST.get(f"question_{question.id}")
+            if selected_choice_id:
+                choice = Choice.objects.get(id=int(selected_choice_id))
+                if choice.check_choice:
+                    correct_count += 1
+
+        return render(request, "aboutMe/test_yourself.html", {
+            "result": f"You answered correctly on {correct_count} from {total_questions} questions!"
+        })
     return render(request, "aboutMe/test_yourself.html")
 
 
